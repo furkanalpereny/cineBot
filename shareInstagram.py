d248ab444e60a91bbd531587750942a02c1c594a
furkanalpereny <furkan.aalperen@gmail.com>
2019-12-16 15:02:31 +0300

    db güncellemesi

--- shareInstagram.py
+++ shareInstagram.py
@@ -1,9 +1,15 @@
 from InstagramAPI import InstagramAPI
+from PIL import Image
 
-InstagramAPI = InstagramAPI("saltstickerco", "herhangibirsifre.1234")
+InstagramAPI = InstagramAPI("unsharedthings", "herhangibirsifre.1234")
 InstagramAPI.login()  # login
 
 photo_path = 'src/img/2019-11-23/19-06-13-12-39-1343139.jpg'
 print(photo_path)
+img = Image.open(open(photo_path, 'rb'))
+img.save(photo_path)
+
 caption = "Sample photo"
-InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+share = InstagramAPI.uploadPhoto(photo=photo_path, caption=caption)
+print(share)
