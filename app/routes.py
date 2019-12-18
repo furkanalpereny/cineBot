a38ff4e143eb3d118fd2c233782bc10a467ecf2b
furkanalpereny <furkan.aalperen@gmail.com>
2019-12-19 00:21:56 +0300

    html render edildi

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import Flask,request, jsonify
 from app import releasedMovies_beyazperde
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 def index():
+    return render_template("main.html")
+@app.route('/getMoviesThisWeek')
+def getMoviesThisWeek():
     movies = releasedMovies_beyazperde.getMoviesThisWeek()
     return jsonify(movies)
