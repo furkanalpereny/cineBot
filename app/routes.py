e1aeb2209e0387b930e6aa5d0bd14305a348fdd3
furkanalpereny <furkan.aalperen@gmail.com>
2019-12-21 00:38:30 +0300

    dinamik sayfa tasarlandı

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,18 @@ from flask import Flask,request, jsonify
 from app import releasedMovies_beyazperde
 from app import app
 from flask import render_template
+import json
 
 @app.route('/')
-def index():
+def main():
     return render_template("main.html")
+
+@app.route('/index')
+def index():
+    with open('src/json/2019-12-21_releasedMovies.json') as json_file:
+        movies = json.load(json_file)
+    return render_template("index.html",movies = movies)
+
 @app.route('/getMoviesThisWeek')
 def getMoviesThisWeek():
     movies = releasedMovies_beyazperde.getMoviesThisWeek()
